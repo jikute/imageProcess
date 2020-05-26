410d89ad96ad23a5498a3ba243b1546d26fdddd0
jikute <54138884+jikute@users.noreply.github.com>
2020-05-25 22:57:39 -0700

    5/25

--- image/main.c
+++ image/main.c
@@ -13,38 +13,6 @@ int main()
 	for (int i = 0; i < row; i++)
 		image[i] = malloc(sizeof(unsigned char) * column);
 	readImage(image, row, column, "triangle.raw");
-	
-	//create the filter
-	int rowF = 41;
-	int columnF = 41;
-	int** gfilter = \
-		(int**)malloc(sizeof(int*) * rowF);
-	for (int i = 0; i < rowF; i++)
-		gfilter[i] = malloc(sizeof(int) * columnF);
-	Gaussian(gfilter,9,20,20,7,7);
-	FILE* fp;
-	int error = 0;
-	error = fopen_s(&fp, "filter.txt", "wb");
-	if (error != 0)
-	{
-		printf("Cannot open outfile\n");
-		exit(1);
-	}
-	for (int x = 0; x < rowF; x++)
-		fwrite(gfilter[x], 4, columnF, fp);
-	fclose(fp);
-	printf("write sucess\n");
-
-	/*
-	for (int x = 0; x < rowF; x++)
-	{
-		for (int y = 0; y < columnF; y++)
-		{
-			printf("%d ", filter[x][y]);
-		}
-		printf("\n");
-	}
-	*/
 
 	// create a buffer to store the operated image
 	int outRow = row;
@@ -60,11 +28,6 @@ int main()
 		free(image[i]);
 	}
 	free(image);
-	for (int i = 0; i < rowF; i++)
-	{
-		free(gfilter[i]);
-	}
-	free(gfilter);
 	writeImage(outImage,outRow,outColumn,"degradedTriangle.raw");
 	// free output buffer
 	for (int i = 0; i < outRow; i++)
